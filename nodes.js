2c7c04cc4c855fd1e09a3e5fcf3a1a3b2bb691e4
Guilherme Santiago <161088039+gs4ntiago@users.noreply.github.com>
2024-03-24 00:03:24 -0300

    first idea

---
+++ nodes.js
@@ -0,0 +1,12 @@
+function instagram(){
+        window.location.href = "https://www.instagram.com/gs4ntiago";
+}
+function twitter(){
+        window.location.href = "https://twitter.com/g_s4nt_";
+}
+function whatsapp(){
+        window.location.href = "https://wa.me/5581986318920";
+}
+function  github(){
+        window.location.href = "https://github.com/gs4ntiago";
+}
